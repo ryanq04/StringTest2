6fba70e43b3cd0ae63468cc3ffcd9fa76a60d3cd
Ryan_qi <ryan2004.qi@gmail.com>
2021-05-05 18:13:33 -0700

    Testing strings out #2

--- src/com/ryan/Main.java
+++ src/com/ryan/Main.java
@@ -2,7 +2,7 @@ package com.ryan;
 
 public class Main {
     public static void main(String[] args) {
-        String myStr = "Hello planet earth, you are a great planet.";
-        System.out.println(myStr.indexOf("planet"));
+        String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        System.out.println(txt.length());
     }
 }
